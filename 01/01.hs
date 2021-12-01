1e3a0ddf1b49c12b9053edc0718de7b515deeabd
Juraj Trappl <juro.trappl@gmail.com>
2021-12-01 10:36:41 +0100

    01 refactor

--- 01/01.hs
+++ 01/01.hs
@@ -5,24 +5,17 @@ parseInput = do
     fContent <- readFile "01.in"
     return $ map (\x -> read x :: Depth) $ lines fContent
 
-depthSweep :: [Depth] -> Int
-depthSweep [x] = 0
-depthSweep (x:rest@(y:ys))
-    | x < y = 1 + depthSweep rest
-    | otherwise = 0 + depthSweep rest
-
-fstPart :: IO ()
-fstPart = do
-    input <- parseInput
-    print $ depthSweep input
-
 windowsOf :: Int -> [a] -> [[a]]
 windowsOf n (x:xs)
     | length xs >= n = (x : take (n - 1) xs) : windowsOf n xs
     | otherwise = [x:xs]
 
+depthSweep :: [Depth] -> Int
+depthSweep = length . filter (\l -> head l < last l) . windowsOf 2
+
+fstPart :: IO ()
+fstPart = parseInput >>= (print . depthSweep)
+
 sndPart :: IO ()
-sndPart = do
-    input <- parseInput
-    print $ depthSweep $ map sum $ windowsOf 3 input
+sndPart = parseInput >>= (print . depthSweep . map sum . windowsOf 3)
 
