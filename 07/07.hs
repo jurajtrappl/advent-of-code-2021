59fefce69481d36d4d57e8436205f1338d853a5f
Juraj Trappl <juro.trappl@gmail.com>
2021-12-07 07:06:04 +0100

    add solution to 7. problem both parts

---
+++ 07/07.hs
@@ -0,0 +1,28 @@
+import Data.List.Split (splitOn)
+import Data.List (sort)
+
+type HorizontalPos = Int
+
+parseInput :: IO [HorizontalPos]
+parseInput = fmap (map (\s -> read s :: HorizontalPos) . splitOn ",") (readFile "07.in")
+
+median :: [HorizontalPos] -> Int
+median pos
+    | even n = div (pos !! div n 2 + pos !! div (n + 1) 2) 2
+    | otherwise = pos !! div (n + 1) 2
+    where n = length pos
+
+countConstantBurnRateFuelCost :: HorizontalPos -> [HorizontalPos] -> Int
+countConstantBurnRateFuelCost bestPos = foldl (\acc p -> acc + abs (p - bestPos)) 0
+
+fstPart :: IO ()
+fstPart = parseInput >>= (\positions -> print $ countConstantBurnRateFuelCost (median $ sort positions) positions)
+
+arithMean :: [HorizontalPos] -> Int
+arithMean pos = floor (fromIntegral (sum pos) / fromIntegral (length pos))
+
+countIncreasingBurnRateFuelCost :: HorizontalPos -> [HorizontalPos] -> Int
+countIncreasingBurnRateFuelCost bestPos = foldl (\acc p -> acc + sum (take (abs (bestPos - p)) [1..])) 0
+
+sndPart :: IO ()
+sndPart = parseInput >>= (\positions -> print $ countIncreasingBurnRateFuelCost (arithMean positions) positions)
