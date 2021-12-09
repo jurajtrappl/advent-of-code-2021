89f38e2d33806cdb3dd19d2724f0160721733e2b
Juraj Trappl <juro.trappl@gmail.com>
2021-12-09 10:43:23 +0100

    add solution to 9. problem the second part

--- 09/09.hs
+++ 09/09.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
-import qualified Data.List as List
+import qualified Data.Graph as Graph
 import qualified Data.Matrix as Matrix
 import Data.Maybe (isJust)
+import Data.List (foldl', sort)
 
 type HeightMap = Matrix.Matrix Int
 
@@ -21,26 +23,48 @@ validPosition :: HeightMap -> Position -> Bool
 validPosition heightMap (Pos { .. }) = isJust $ Matrix.safeGet row col heightMap
 
 getNeighbours :: HeightMap -> Position -> [Position]   -- up, left, right, down
-getNeighbours heightmap center@(Pos { .. }) = filter (validPosition heightmap) positionsAround
+getNeighbours heightmap (Pos { .. }) = filter (validPosition heightmap) positionsAround
     where positionsAround = [Pos (row - 1) col, Pos row (col - 1), Pos row (col + 1), Pos (row + 1) col]
 
 valueFromPos :: HeightMap -> Position -> Int
 valueFromPos heightMap (Pos{ .. }) = heightMap Matrix.! (row, col)
 
 areSmaller :: HeightMap -> Position -> Bool
-areSmaller heightMap p@(Pos { .. }) = all (> heightMap Matrix.! (row, col)) neighboursValues
-    where neighbours = getNeighbours heightMap p
-          neighboursValues = map (valueFromPos heightMap) neighbours
+areSmaller heightMap p@(Pos { .. }) = let neighboursValues = map (valueFromPos heightMap) $ getNeighbours heightMap p
+                                          value = heightMap Matrix.! (row, col)
+                                      in all (> value) neighboursValues
 
-findSmaller :: HeightMap -> [Position] -> [Int]
-findSmaller heightMap = map (\(_, p) -> valueFromPos heightMap p) . filter fst . map (\p -> (areSmaller heightMap p, p))
+findSmaller :: HeightMap -> [Position] -> [Position]
+findSmaller heightMap = map snd . filter fst . map (\p -> (areSmaller heightMap p, p))
 
 toRiskLevels :: [Int] -> [Int]
 toRiskLevels = map (+1)
+
+allPositions :: HeightMap -> [Position]
+allPositions heightMap = [Pos r c | r <- [1..Matrix.nrows heightMap], c <- [1..Matrix.ncols heightMap]]
 
 fstPart :: IO ()
 fstPart = do
     input <- parseInput
     let heightMap = Matrix.fromLists input
-    let lowPoints = findSmaller heightMap [Pos r c | r <- [1..Matrix.nrows heightMap], c <- [1..Matrix.ncols heightMap]]
-    print $ sum $ toRiskLevels lowPoints
+    let lowPoints = findSmaller heightMap (allPositions heightMap)
+    print $ sum $ toRiskLevels $ map (valueFromPos heightMap) lowPoints
+
+searchForId :: [(Position, Int)] -> Position -> Int
+searchForId idsWithPos pos = snd $ head $ filter (\(p, id) -> p == pos) idsWithPos
+
+connectToNeighbours :: HeightMap -> [(Position, Int)] -> Position -> [Graph.Edge]
+connectToNeighbours heightMap positionsIds pos = map (searchForId positionsIds pos,) neighbourIds
+    where validNeighbours = filter (\(Pos { .. }) -> heightMap Matrix.! (row, col) /= 9) $ getNeighbours heightMap pos
+          neighbourIds = map (searchForId positionsIds) validNeighbours
+
+sndPart :: IO ()
+sndPart = do
+    input <- parseInput
+    let heightMap = Matrix.fromLists input
+    let positions = allPositions heightMap
+    let positionsIds = zip positions [1..length positions]
+    let edges = concatMap (connectToNeighbours heightMap positionsIds) positions
+    let graph = Graph.buildG (1, snd $ last positionsIds) edges
+    let lowPointsIds = map (searchForId positionsIds) $ findSmaller heightMap positions
+    print $ foldl' (*) 1 $ take 3 $ foldl' (flip (:)) [] $ sort $ map (length . Graph.reachable graph) lowPointsIds
