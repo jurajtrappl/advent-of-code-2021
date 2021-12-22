d35d21dc81301a6693d513d061c9bb6be29186b9
Juraj Trappl <juro.trappl@gmail.com>
2021-12-22 15:58:30 +0100

    better DP

--- 15/15.hs
+++ 15/15.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as Map
 import qualified Data.Matrix as Matrix
 import Data.Maybe (isJust, fromJust)
 import Data.Functor ((<&>))
@@ -6,7 +7,7 @@ import Data.List (foldl')
 type Location = (Int, Int)
 type RiskLevel = Integer
 type RiskLevelMap = Matrix.Matrix Int
-type SumMap = Matrix.Matrix Int
+type SumMap = Map.Map (Int, Int) Int
 
 unsafeReadInt :: String -> Int
 unsafeReadInt s = read s :: Int
@@ -18,23 +19,24 @@ indices :: RiskLevelMap -> [Location]
 indices riskLevelMap = [(x, y) | x <- [1..Matrix.nrows riskLevelMap], y <- [1..Matrix.ncols riskLevelMap]]
 
 isValidLocation :: Location -> SumMap -> Bool
-isValidLocation (x, y) = isJust . Matrix.safeGet x y
+isValidLocation location = isJust . Map.lookup location
 
 getGridNeighbours :: SumMap -> Location -> [Location]
 getGridNeighbours sumMap l@(x, y) = filter (`isValidLocation` sumMap) [(x, y - 1), (x - 1, y)]
 
 modifyLocation :: RiskLevelMap -> SumMap -> Location -> SumMap
-modifyLocation riskLevelMap sumMap l@(x, y) = Matrix.setElem newValue l sumMap
-    where neighboursValues = map (sumMap Matrix.!) $ getGridNeighbours sumMap l
+modifyLocation riskLevelMap sumMap l@(x, y) = Map.insert l newValue deleted
+    where neighboursValues = map (sumMap Map.!) $ getGridNeighbours sumMap l
           newValue = if null neighboursValues then 0 else minimum neighboursValues + riskLevel
           riskLevel = riskLevelMap Matrix.! l
+          deleted = Map.delete l sumMap
 
 fstPart :: IO ()
 fstPart = do
     riskLevelMap <- parseInput
-    let sumMap = Matrix.zero (Matrix.ncols riskLevelMap) (Matrix.nrows riskLevelMap)
+    let sumMap = Map.fromList $ zip (indices riskLevelMap) $ replicate (Matrix.nrows riskLevelMap * Matrix.ncols riskLevelMap) 0
     let summed = foldl (modifyLocation riskLevelMap) sumMap (indices riskLevelMap)
-    print $ summed Matrix.! (Matrix.nrows sumMap, Matrix.ncols sumMap)
+    print $ snd $ last (Map.toList summed)
 
 addOne :: RiskLevelMap -> RiskLevelMap
 addOne = fmap (\e -> if e == 9 then 1 else if e == 8 then 9 else mod (e + 1) 9)
@@ -56,6 +58,6 @@ sndPart :: IO ()
 sndPart = do
     riskLevelMap <- parseInput
     let extendedRiskLevelMap = constructGrid riskLevelMap
-    let sumMap = Matrix.zero (Matrix.ncols extendedRiskLevelMap) (Matrix.nrows extendedRiskLevelMap)
+    let sumMap = Map.fromList $ zip (indices riskLevelMap) $ replicate (Matrix.nrows extendedRiskLevelMap * Matrix.ncols extendedRiskLevelMap) 0
     let summed = foldl' (modifyLocation extendedRiskLevelMap) sumMap (indices extendedRiskLevelMap)
-    print $ summed Matrix.! (Matrix.nrows sumMap, Matrix.ncols sumMap)
+    print $ snd $ last (Map.toList summed)
